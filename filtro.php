5782c744511952b93ad6d302afc2f800f4df7807
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2022-10-04 18:48:07 -0300

    first commit vscode

---
+++ filtro.php
@@ -0,0 +1,19 @@
+<?php
+
+function traerFiltro($i)
+{
+    $valor = $_GET['valor'];
+    $db = new PDO('mysql:host=localhost;' . 'dbname=db_catalogo;charset=utf8', 'root', '');
+    $query = $db->prepare('SELECT * FROM producto WHERE tipo = echo $valor;');
+    $query->execute();
+    $productos_tipo = $query->fetchAll(PDO::FETCH_OBJ);
+    return $productos_tipo;
+}
+function traerItems()
+{
+    $db = new PDO('mysql:host=localhost;' . 'dbname=db_catalogo;charset=utf8', 'root', '');
+    $query = $db->prepare('SELECT * FROM producto');
+    $query->execute();
+    $productos = $query->fetchAll(PDO::FETCH_OBJ);
+    return $productos;
+}
