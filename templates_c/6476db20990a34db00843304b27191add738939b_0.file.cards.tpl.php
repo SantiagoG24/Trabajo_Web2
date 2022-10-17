dca7f54ef899a7c5b53149752e858058d6c0d247
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2022-10-17 16:36:31 -0300

    commit

--- templates_c/6476db20990a34db00843304b27191add738939b_0.file.cards.tpl.php
+++ templates_c/6476db20990a34db00843304b27191add738939b_0.file.cards.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-10-15 01:31:29
+/* Smarty version 4.2.1, created on 2022-10-17 21:35:11
   from 'C:\xampp\Proyectos\htdocs\WEB-2\Trabajo_Web2\templates\cards.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_6349f15161a6c3_96244840',
+  'unifunc' => 'content_634dae6fdaa6d2_31172785',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '6476db20990a34db00843304b27191add738939b' => 
     array (
       0 => 'C:\\xampp\\Proyectos\\htdocs\\WEB-2\\Trabajo_Web2\\templates\\cards.tpl',
-      1 => 1665790181,
+      1 => 1666032147,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_6349f15161a6c3_96244840 (Smarty_Internal_Template $_smarty_tpl) {
+function content_634dae6fdaa6d2_31172785 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 <div class="contenedor">
